42fd2ab28393f5f65752b39fd68259ad15b8f2b0
cactiw <knyazev1514@gmail.com>
2020-02-09 23:48:57 +0300

    Added quests integration to Player class

--- libs/Player.py
+++ libs/Player.py
@@ -1,9 +1,11 @@
 from sqlalchemy import Table, Column, ForeignKey, INT, VARCHAR, DATE, ARRAY, TEXT, BOOLEAN, BIGINT, FLOAT, func, and_, \
     or_
 from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship, backref, Session
 
-from work_materials.globals import Base
+from libs.Quest import Quest
+
+from work_materials.globals import Base, dispatcher, Session as NewSession
 
 
 class Player(Base):
@@ -15,11 +17,51 @@ class Player(Base):
     pair_id = Column(INT, ForeignKey('players.id'))
     pair = relationship("Player", uselist=False, backref=backref('parent', remote_side=[id]))
 
+    quest_id = Column(INT)
+    selected = Column(BOOLEAN, default=False)
     progress = Column(JSONB)
     status = Column(VARCHAR)
     battle_status = Column(VARCHAR)
 
     hp = Column(INT)
     max_hp = Column(INT)
+
+    #
+
+    def set_game_class(self, new_class):
+        self.game_class = new_class
+
+    def update(self, session: Session):
+        session.add(self)
+        session.commit()
+
+
+    def check_has_pair_selected(self):
+        if self.pair is None:
+            raise RuntimeError
+        return self.pair.selected
+
+    def set_quest(self, quest_id, session: Session):
+        self.quest_id = quest_id
+        self.selected = False
+        self.status = "quest"
+
+        quest = Quest.get_quest(quest_id)
+        quest.start(self)
+        dispatcher.bot.send_message(chat_id=self.id, text=quest.get_enter_text(), parse_mode='HTML')
+        self.update(session)
+
+
+    def progress_both_to_quest(self, quest_id, session: Session):
+        self.set_quest(quest_id, session)
+        self.pair.set_quest(quest_id, session)
+
+
+
+
+
+
+
+
 
 
