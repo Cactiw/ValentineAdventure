35715e75e7e2beb70d128a45d2e7f4ba55a90720
Werozel <khaibulaev.mn@gmail.com>
2020-02-13 09:29:08 +0300

    Написал базовые функции в класс ItemRel

--- libs/Item.py
+++ libs/Item.py
@@ -1,8 +1,8 @@
-from work_materials.globals import Base
+from work_materials.globals import Base, Session
 from sqlalchemy import Column, INT, ForeignKey, UniqueConstraint, VARCHAR
 from sqlalchemy.orm import relationship, backref
 
-import logging
+from libs.Player import Player
 
 
 class ItemRel(Base):
@@ -16,6 +16,30 @@ class ItemRel(Base):
     __table_args__ = (
         UniqueConstraint('item_id', 'player_id', name='unique_record')
     )
+
+    def reduce_quantity(self, n=1):
+        if self.quantity == n:
+            # TODO Удалить из таблицы
+            pass
+        elif self.quantity < n:
+            raise ValueError('Not enough items!')
+        else:
+            self.quantity -= n
+
+    def increase_quantity(self, n=1):
+        self.quantity += n
+
+    def get_inventory(self, player):
+        if isinstance(player, int):
+            id = player
+        elif isinstance(player, Player):
+            id = player.id
+        else:
+            raise TypeError('player is not int nor class Player')
+
+        return Session().query(ItemRel.item_id, ItemRel.quantity).filter_by(player_id=id)
+
+
 
 
 class Item(Base):
